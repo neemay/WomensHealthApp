767bed4e1061541800c6e55801a07130d9ec705f
Yarden <yardenn1@gmail.com>
2019-03-07 11:35:42 -0500

    playing around with mongo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+//var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/public/views'));
@@ -21,5 +22,31 @@ app.get('/profile', function(req, res) {
 app.get('/history', function(req, res) {
     res.render('pages/history', { title: 'My History' });
 });
+
+//Connect to the Mongo Database
+//mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true });
+
+//Set up the database schema
+//var Schema = mongoose.Schema;
+
+//Save the whole tweet object in the database for extensibility
+//var dbSchema = new Schema({
+//  username: String
+//});
+
+//Create a model using this schema
+//var User = mongoose.model('User', dbSchema);
+//module.exports = User;
+
+//app.get('/addToDB', function(req, res) {
+//  var new_user = new User({
+//    username: "Yarden"
+//  });
+//  new_user.save(function(err) {
+//    if(err) throw err;
+//  });
+//  console.log("Saved to DB");
+//  res.send("wee");
+//});
 
 app.listen(3000, () => console.log('Server listening on port 3000.'));
--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -1,6 +1,15 @@
 var app = angular.module("obie", []);
 app.controller('controller', function ($scope, $http) { 
+  //Scope variable initialization
   $scope.isDashboard = false;
   $scope.isProfile = false;
   $scope.isHistory = false;
+  $scope.passwordError = false;
+  
+  //Function to trigger account creation
+  $scope.createAccount = function() {
+    if($scope.createPassowrd != $scope.confirmPassword) {
+      $scope.passwordError = true;
+    }
+  }
 });
