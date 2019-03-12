f1f7c3aa76e0ea0c353c98dcfe7abd4c68016bc9
Yarden <yardenn1@gmail.com>
2019-03-12 15:14:45 -0400

    Added logout code to all pages

--- public/resources/js/history.js
+++ public/resources/js/history.js
@@ -1,6 +1,15 @@
 var app = angular.module("obie", []);
-app.controller('controller', function ($scope, $http) { 
+app.controller('controller', function ($scope, $http, $window) { 
   $scope.isDashboard = false;
   $scope.isProfile = false;
   $scope.isHistory = true;
+  
+  $scope.logout = function() {
+    $http({
+      method: 'GET',
+      url: '/logout'
+    }).success(function(response) {
+      $window.location.href = '/';
+    });
+  }
 });
--- public/resources/js/profile.js
+++ public/resources/js/profile.js
@@ -1,6 +1,15 @@
 var app = angular.module("obie", []);
-app.controller('controller', function ($scope, $http) { 
+app.controller('controller', function ($scope, $http, $window) { 
   $scope.isDashboard = false;
   $scope.isProfile = true;
   $scope.isHistory = false;
+  
+  $scope.logout = function() {
+    $http({
+      method: 'GET',
+      url: '/logout'
+    }).success(function(response) {
+      $window.location.href = '/';
+    });
+  }
 });
